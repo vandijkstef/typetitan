b87bbefefc35aaa802df5a107c897bb1ebf7fae7
Stef van Dijk <stef@vandijkstef.nl>
2018-04-02 17:07:31 +0200

    rearranged/rewrote

--- server/app.js
+++ server/app.js
@@ -1,62 +1,50 @@
 const path = require('path');
 
 const express = require('express');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+const eSession = require('express-session');
+const FileStore = require('session-file-store')(eSession);
 const compression = require('compression');
-const ws = require('ws').Server;
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // var favicon = require('serve-favicon');
 
-// Require all routes
-const index = require('./routes/index');
-
-// Setup websocket
-const wss = new ws({port: 13375});
-wss.on('connection', (ws, req) => {
-
-	// Fetch connection session ID
-	const value = '; ' + req.headers.cookie;
-	const parts = value.split('; ' + 'connect.sid' + '=');
-	const sessionID = parts.pop().split(';').shift();
-
-	ws.on('message', (message) => {
-		console.log('received', message);
-		if (message === 'wantScores') {
-			ws.send('heres some scores');
-		}
-	});
-});
 // Require our websocket. This will setup the websocket on it's own
 require('./socket.js');
 
 // Setup App
 const app = express();
 
+// Add session support
+const session = eSession({
+	secret: 'hAF38J5ja',
+	store: new FileStore(),
+	saveUninitialized: true,
+	resave: false
+});
+app.use(session);
+
 // Enable GZIP
 app.use(compression());
+
+// Enable form data and cookies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Add session support
-app.use(session({
-	secret: 'hAF38J5ja',
-	store: new FileStore(),
-	saveUninitialized: true,
-	resave: false
-}));
-
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); // uncomment after placing your favicon in /public
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
+// Require all routes
+const routes = {
+	index: require('./routes/index')
+};
+app.use('/', routes.index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
